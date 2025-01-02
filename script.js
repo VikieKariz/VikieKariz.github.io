dbc0e28bdfd34e7e8dcea0bd26b63446d2115ea9
VikieKariz <vikieqariz@gmail.com>
2025-01-02 23:06:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+console.log('Hello World!');
+document.getElementById('whatsappShareButton').onclick = function() {
+    var websiteURL = encodeURIComponent(window.location.href);
+    var message = encodeURIComponent('beib checkout this website i designed for you: ');
+    var whatsappURL = 'https://api.whatsapp.com/send?text=' + message + websiteURL;
+    window.open(whatsappURL, '_blank');
+}
 
